6d0b153c57a1705ea4c03db6fbdad91ff17b14c8
Ha Tran <hatran3887@googlemail.com>
2026-06-23 11:23:56 +0200

    Add data with json and implement index route to show all posts

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    """
+    The index route, which shows all the blog posts
+    """
+    with open('storage/blog_posts.json') as f:
+        blog_posts = json.load(f)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
