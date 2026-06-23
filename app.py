025539f48de2a63c14d7aa5de90f000213933a59
Ha Tran <hatran3887@googlemail.com>
2026-06-23 13:27:19 +0200

    Add update function to update an existing post.

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
 
+def fetch_post_by_id(post_id):
+    """
+    Fetch a post by its id
+    """
+    with open('storage/blog_posts.json', 'r') as f:
+        posts = json.load(f)
+    return next((post for post in posts if post['id'] == post_id), None)
+
+
 @app.route('/')
 def index():
     """
@@ -43,16 +52,43 @@ def delete(post_id):
     """
     The delete route deletes the post with given id and redirects to the index route
     """
-    if not isdigit(post_id):
-        print('Invalid post id')
-        return redirect(url_for('index'))
-
     with open('storage/blog_posts.json', 'r') as f:
         posts = json.load(f)
     posts = [post for post in posts if post['id'] != int(post_id)]
     with open('storage/blog_posts.json', 'w') as f:
         json.dump(posts, f)
     return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    """
+    The update route for both GET and POST methods, the GET method shows the form for the post to update
+    and the POST method save the post to JSON file and redirect user to the index route
+    """
+    # Fetch the blog posts from the JSON file
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        author = request.form.get('author')
+        content = request.form.get('content')
+
+        with open('storage/blog_posts.json', 'r') as f:
+            posts = json.load(f)
+        for i, post in enumerate(posts):
+            if post['id'] == int(post_id):
+                posts[i]['title'] = title
+                posts[i]['author'] = author
+                posts[i]['content'] = content
+        with open('storage/blog_posts.json', 'w') as f:
+            json.dump(posts, f)
+
+        return redirect(url_for('index'))
+    return render_template('update.html', post=post)
 
 
 if __name__ == '__main__':
